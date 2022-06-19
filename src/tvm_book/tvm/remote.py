2bd98f0bda75cdf46c27d07649cdcb630e2345bb
xinetzone <xinzone@outlook.com>
2022-06-19 22:12:57 +0800

    modified:   doc/tutorials/d2l-tvm/getting_started/remote.ipynb

--- src/tvm_book/tvm/remote.py
+++ src/tvm_book/tvm/remote.py
@@ -2,11 +2,25 @@ from tvm.contrib.utils import TempDirectory
 
 
 class TempModule(TempDirectory):
-    def export_library(self, module, filename):
+    """Create temp dir which deletes the contents when exit.
+
+    Parameters
+    ----------
+    custom_path : str, optional
+        Manually specify the exact temp dir path
+
+    keep_for_debug : bool
+        Keep temp directory for debugging purposes
+    Returns
+    -------
+    temp : TempDirectory
+        The temp directory object
+    """
+    def export_library(self, module, filename, *args, **kwargs):
         """将该 module 保存在本地临时文件夹中
         """
         path = self.relpath(filename)
-        module.export_library(path)
+        module.export_library(path, *args, **kwargs)
         return path
 
     def upload(self, remote, module, filename):
@@ -15,7 +29,4 @@ class TempModule(TempDirectory):
         remote.upload(path)
         # remote_module
         return remote.load_module(filename)
-
-    # def packed_func(self, remote_module, ctx):
-    #     remote_fuc = remote_module["default"]
-    #     remote_rt =  graph_executor.GraphModule(remote_fuc(ctx))
+        
