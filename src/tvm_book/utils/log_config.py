d1bd2d5682cf198f0f2c0bc20f13ae296a16805a
liuxinwei <liuxinwei@xmsilicon.cn>
2023-12-12 15:29:46 +0800

    更新 	修改：     src/tvm_book/utils/log_config.py

--- src/tvm_book/utils/log_config.py
+++ src/tvm_book/utils/log_config.py
@@ -1,9 +1,12 @@
 import logging
 
+
 def config_logging(
     filename,
+    logger_name="logger",
     file_formatter="%(levelname)s|%(asctime)s|%(name)s| -> %(message)s|%(module)s.%(funcName)s@%(pathname)s:%(lineno)d",
-    stream_formatter="%(levelname)s|%(asctime)s -> %(message)s"):
+    stream_formatter="%(levelname)s|%(asctime)s -> %(message)s",
+    ):
     """配置 logging
 
     Args:
@@ -11,18 +14,14 @@ def config_logging(
         file_formatter: 日志文件配置
         stream_formatter: 控制台打印配置
     """
-    # 创建文件处理程序，并记录调试消息
-    fh = logging.FileHandler(filename)
-    fh.setLevel(logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG,
+                        format=file_formatter,
+                        datefmt='%m-%d %H:%M',
+                        filename=filename,
+                        filemode='a')
     # 创建日志级别更高的控制台处理程序
     ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO) # 或者logging.ERROR
-    # 创建formatter并将其
-    fh_formatter = logging.Formatter(file_formatter)
+    ch.setLevel(logging.INFO) # 或者 logging.ERROR
     ch_formatter = logging.Formatter(stream_formatter)
-    fh.setFormatter(fh_formatter)
     ch.setFormatter(ch_formatter)
-    logging.basicConfig(
-        level=logging.DEBUG,
-        handlers=[fh, ch]
-    )
+    logging.getLogger("").addHandler(ch)
