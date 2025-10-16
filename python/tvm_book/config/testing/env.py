deee722761f09222894f6bd60ace93244a304e20
liuxinwei <liuxinwei@xmsilicon.cn>
2025-10-16 19:48:42 +0800

    refactor(tvm_book): 重构配置文件结构并更新文档
    - 将 env.py 和 cuda_env.py 移动到 testing 子目录
    - 更新 TVM-FFI 教程文档内容
    - 删除不再使用的示例文件 a.py
    - 优化 python-guide.ipynb 中的描述文本

--- python/tvm_book/config/testing/env.py
+++ python/tvm_book/config/testing/env.py
@@ -30,22 +30,3 @@ def set_caffeproto(caffeproto_root: str|Path="../../../tests/caffeproto"):
     CaffeProto_PATH = str(caffeproto_root/'python')
     if CaffeProto_PATH not in sys.path:
         sys.path.extend([CaffeProto_PATH])
-
-# def set_mxnet():
-#     os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
-#     os.environ['MXNET_CUDNN_LIB_CHECKING'] = '0'
-
-
-# def set_cudnn(cuda_path:str|Path="/usr/local/cuda/bin",
-#               LD_LIBRARY_PATH:str|Path="/usr/local/cuda/lib64"):
-#     os.environ["PATH"] += f":{cuda_path}"
-#     os.environ["LD_LIBRARY_PATH"] = LD_LIBRARY_PATH
-
-
-# def import_tvm(tvm_root):
-#     from importlib import import_module
-#     set_tvm(tvm_root)
-#     # import 模块
-#     tvm = import_module('tvm')
-#     vta = import_module('vta')
-#     return tvm, vta
